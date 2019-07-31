2394eb490e3097a5a800b98ccac04991508f03d0
Caoimhan Bradley <caoimhan@alchemytechs.com>
2019-07-31 14:14:37 +0100

    Add project files.

---
+++ Methods/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHi("Mike", 33);
+            SayHi("John", 22);
+            SayHi("Tom", 18);
+
+            Console.ReadLine();
+        }
+
+        static void SayHi(string name, int age)
+        {
+            Console.WriteLine("Hello " + name + " you are " + age);
+
+        }
+
+    }
+}
